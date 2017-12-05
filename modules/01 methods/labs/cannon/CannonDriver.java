3f74ca44b7a3c7fb515ed4b377133b3607fed4ab
jeremy <jiverson002@csbsju.edu>
2017-12-05 11:42:23 -0600

    starting to convert labs to use driver program paradigm

---
+++ modules/01 methods/labs/cannon/CannonDriver.java
@@ -0,0 +1,27 @@
+package cannon;
+
+// height, distance, timeToHit, distanceToHit
+import static cannon.Physics.*;
+
+/**
+ * A program that displays a table of a cannon ball's travel from firing until
+ * it hits the ground for a specified initial velocity and angle.
+ *
+ * @author A template by CSB|SJU Computer Science faculty members
+ */
+public class CannonDriver
+{
+  /**
+   * The is where the program starts.
+   */
+  public static void main(String [] args)
+  {
+    double v0 = 15;
+    double A  = 45;
+
+    //*** Ignore this loop in Part 1!
+    for (int i=0; i<=20; i++)
+    {
+    }
+  }
+}
---
+++ modules/01 methods/labs/cannon/Physics.java
@@ -0,0 +1,32 @@
+package cannon;
+
+// sin, cos, toRadians
+import static java.lang.Math.*;
+
+public class Physics
+{
+  /**
+   * Acceleration due to gravity at sea level on earth.
+   */
+  private static final double GRAVITY = -9.80665;
+
+  /**
+   * Computes the height of a cannon ball shot from the ground at initial
+   * velocity v0, angle A at t seconds after the shot
+   *
+   * @param v0 the initial velocity in units/second (could be any unit of
+   *           length)
+   * @param A  the firing angle in degrees
+   * @param t  the elapsed time since firing in seconds
+   *
+   * @return   the computed value of the height
+   */
+  public static double height(double v0, double A, double t)
+  {
+    //*** code to compute the value here***
+    return 0; //*** replace zero with the computed value
+  }
+
+  //*** Put the remaining two specified functions here using height as a model
+  //*** Be sure to include proper Javadoc comments like the one above
+}
